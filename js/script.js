0b2c39217306ebd31da2360e7a8e31ccc0f60127
gleicekelly13 <gleice.kelly22@hotmail.com>
2022-12-05 15:41:32 -0300

    Criação dos parágrafos com informações dentro das divs

--- js/script.js
+++ js/script.js
@@ -38,18 +38,33 @@ const data = [
 ];
 
 // Seleção de elementos
-const imcTabe = document.querySelector("#imc-table");
+const imcTable = document.querySelector("#imc-table");
 
 const heightInput = document.querySelector("#height");
 const weightInput = document.querySelector("#weight");
 const calcBtn = document.querySelector("#calc-btn");
 const clearBtn = document.querySelector("#clear-btn");
 
-//Funções Criação da tabela
+//Funções 
 function createTable(data) {
     data.forEach((item)) => {
+      const div = document.createElement("div");
+      div.classList.add("table-data");
+      
+      // Parágrafos com informações dentro das divs
+      const classification = document.createElement("p");
+      classification.innerText = item.classification;
 
-        
+      const info = document.createElement("p");
+      info.innerText = item.info;
 
+      const obesity = document.createElement("p");
+      obesity.innerText = item.obesity;
+
+      div.appendChild(classification);
+      div.appendChild(info);
+      div.appendChild(obesity);
+
+      imcTable.appendChild(div);
     }
 }
