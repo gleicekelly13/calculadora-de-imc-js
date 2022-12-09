56317cac18dcfef601b121cfbfe9e9801b9e6062
gleicekelly13 <gleice.kelly22@hotmail.com>
2022-12-09 15:58:41 -0300

    Exibindo feedback visual

--- js/script.js
+++ js/script.js
@@ -132,6 +132,29 @@ calcBtn.addEventListener("click", (e) =>{
     imcNumber.innerText = imc;
     imcInfo.innerText = info;
 
+    switch (info) {
+        case "Magreza":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Normal":
+            imcNumber.classList.add("good");
+            imcInfo.classList.add("good");
+            break;
+        case "Sobrepeso":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Obesidade":
+            imcNumber.classList.add("medium");
+            imcInfo.classList.add("medium");
+            break;
+        case "Obesidade grave":
+            imcNumber.classList.add("high");
+            imcInfo.classList.add("high");
+            break;
+    }
+
     showOrHideResults();
 });
 
